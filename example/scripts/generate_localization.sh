bedcdf88034aa7cb2da85eb08317ab6491bc0f73
christian <kwado.tech@gmail.com>
2023-02-04 03:57:40 +0200

    Cleaned-up script.

--- example/scripts/generate_localization.sh
+++ example/scripts/generate_localization.sh
@@ -4,6 +4,8 @@ chmod +x ./replace_string.sh
 # generate localization
 (cd ../ && flutter gen-l10n)
 
+filePath="../lib/localization/gen-l10n/app_localizations.dart"
+searchParameter="abstract class AppLocalizations {"
 requiredImports=$(cat << EOM
 import 'package:localization_mapper_annotation\/localization_mapper_annotation.dart';\n\
 part 'app_localizations.g.dart';\n\n\
@@ -12,11 +14,9 @@ abstract class AppLocalizations {
 EOM
 )
 
-# requiredImports="import \'package:localization_mapper_annotation\/localization_mapper_annotation.dart'\;\npart \'app_localizations.g.dart'\;\n\n@LocalizationMapperAnnotation()\nabstract class AppLocalizations {"
-
 # write imports and annotations to app_localization.dart file
 echo "\nAdding required imports to generated app_localizations"
-bash ./replace_string.sh "../lib/localization/gen-l10n/app_localizations.dart" "abstract class AppLocalizations {" "$requiredImports"
+bash ./replace_string.sh "$filePath" "$searchParameter" "$requiredImports"
 
 echo "\nGenerating app_localizations mapper files"
 (cd ../ && flutter pub run build_runner build --delete-conflicting-outputs)
